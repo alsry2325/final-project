194471e178620888c7dd4173edce08e8fdfd1f3d
jooseohyeon <wntjguswkd@gmail.com>
2023-11-27 23:06:03 +0900

    feat/#2/전자결재 등록 도전,,2

--- src/main/java/com/playwithcode/businessbridge/approval/dto/request/ApprovalCreateRequest.java
+++ src/main/java/com/playwithcode/businessbridge/approval/dto/request/ApprovalCreateRequest.java
@@ -6,4 +6,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public class ApprovalCreateRequest {
+
+
+
 }
--- src/main/java/com/playwithcode/businessbridge/approval/service/ApprovalService.java
+++ src/main/java/com/playwithcode/businessbridge/approval/service/ApprovalService.java
@@ -14,6 +14,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.playwithcode.businessbridge.approval.domain.type.ApprovalStatusType.ACTIVATE;
 
 @Service
@@ -40,5 +43,9 @@ public class ApprovalService {
 
     /* 2. 새 결재 작성(결재 등록) */
     public void save(ApprovalCreateRequest approvalCreateRequest, CustomUser customUser) {
+
+        List<Approver> approvers = new ArrayList<>();
+
+//        for(int i = 0; i < approvalCreateRequest);
     }
 }
