937c62bb22ad9cd587e308d5a60073525b7b6cb2
alsry2325 <alsry2325@naver.com>
2023-11-29 14:38:58 +0900

    #7 로그인 이슈 수정

--- src/main/java/com/playwithcode/businessbridge/jwt/CustomUser.java
+++ src/main/java/com/playwithcode/businessbridge/jwt/CustomUser.java
@@ -7,16 +7,16 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Getter
 public class CustomUser extends User { //멤버코드도 필요하기 때문에
 
-    private final Long memberCode;
+    private final Long emplyCode;
 
-    public CustomUser(Long memberCode, UserDetails userDetails) {
+    public CustomUser(Long emplyCode, UserDetails userDetails) {
         super(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
-        this.memberCode = memberCode;
+        this.emplyCode = emplyCode;
     }
 
-    public static CustomUser of(Long memberCode, UserDetails userDetails) {
+    public static CustomUser of(Long emplyCode, UserDetails userDetails) {
         return new CustomUser(
-                memberCode,
+                emplyCode,
                 userDetails
         );
     }
