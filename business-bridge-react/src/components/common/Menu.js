84dfd01a00c41718fbf7060ef1021a0e8def5c86
alsry2325 <alsry2325@naver.com>
2023-12-02 19:12:10 +0900

    #13 navar구현 완료

---
+++ business-bridge-react/src/components/common/Menu.js
@@ -0,0 +1,62 @@
+import {NavLink} from "react-router-dom";
+function Menu() {
+
+    return (
+        <>
+            <div className="Navbars">
+                <ul className="NavbarWrappers">
+                    <li className="NavImage">
+                        <NavLink className="Menu-Image-myPage" to="/sign-up">
+                            <img className="Menu-Image-myPage" src="/images/employee-image.png" alt="My Image" />
+                        </NavLink>
+                    </li>
+                    <li className="NavbarElement">
+                        <strong>정민교사원님</strong>
+                    </li>
+                    <li className="NavbarElement">
+                        <NavLink className="link" to="/">
+                            홈
+                        </NavLink>
+                    </li>
+                    <li className="NavbarElement">
+                        <NavLink className="link" to="/about-us">
+                            주소록
+                        </NavLink>
+                    </li>
+                    <li className="NavbarElement">
+                        <NavLink className="link"to="/contact-us">
+                            쪽지
+                        </NavLink>
+                    </li>
+                    <li className="NavbarElement">
+                        <NavLink className="link" to="/sign-in">
+                           캘린더
+                        </NavLink>
+                    </li>
+                    <li className="NavbarElement">
+                        <NavLink className="link" to="/sign-in">
+                            공지사항
+                        </NavLink>
+                    </li>
+                    <li className="NavbarElement">
+                        <NavLink className="link" to="/sign-in">
+                            전자결제
+                        </NavLink>
+                    </li>
+                    <li className="NavbarElement">
+                        <NavLink className="link" to="/sign-in">
+                            고객관계관리
+                        </NavLink>
+                    </li>
+                    <li className="NavbarElement">
+                        <NavLink className="link" to="/sign-in">
+                            캘린더
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+        </>
+    );
+}
+
+export default Menu;
--- business-bridge-react/src/layouts/Layout.js
+++ business-bridge-react/src/layouts/Layout.js
@@ -1,13 +1,15 @@
 import Header from "../components/common/Header";
-import Navbar from "../components/common/Navbar";
 import {Outlet} from "react-router-dom";
+import {useState} from "react";
+import Menu from "../components/common/Menu";
+import '../css/Menu.css'
 
 function Layout() {
-
+    const[clicked,isClicked] = useState(false)
     return (
         <>
-            <Header/>
-            <Navbar/>
+            <Header clicked={clicked} isClicked={isClicked}/>
+            {clicked?<Menu/>:null}
             <main>
                 <Outlet/>
             </main>
