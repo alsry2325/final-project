ba395727e986675f38261ed8293ffd83fe495b7f
jung su hyun <bm4564@naver.com>
2023-12-11 16:54:09 +0900

    feat#7 sales 영업목록 css 수정

--- business-bridge-react/src/apis/SalesAPICalls.js
+++ business-bridge-react/src/apis/SalesAPICalls.js
@@ -1,27 +1,46 @@
 import {authRequest, request} from "./Api";
-import {getSalesList, getSales, postSuccess} from "../modules/SalesModule";
-import {useDispatch, useSelector} from "react-redux";
+import {getSalesList
+    , getSales
+    , postSuccess
+    , putSuccess
+    , deleteSuccess
+    , getSalesProductList
+    , getSalesStatistics
+
+} from "../modules/SalesModule";
+import {toast} from "react-toastify";
 
 //영업 등록
 export const callSalesRegistAPI = ({ registRequest }) => {
-
+    console.log("영업등록 보내기전의 값");
+    console.log(registRequest);
     return async (dispatch, getState) => {
+        const result = await request('POST' , `/api/v1/sales`
+            , {'Content-Type' : 'application/json'}
+            , registRequest
+        )
 
-        const result = await authRequest.post('/api/v1/sales',
-            JSON.stringify(registRequest),
-            {
-                headers : {
-                    'Content-Type' : 'application/json'
-                }
-            }).catch(e => {
-                console.log("api 요청결과");
-                console.log(e)
-        });
-
-        console.log('callSalesRegistAPI result : ', result);
-
-        if(result?.status === 201) {
+        console.log('callSalesRegistAPI result.status >>>>>>>>>>>>> '+result.status);
+        if(result.status === 201) {
             dispatch(postSuccess());
+            toast.info("영업 등록이 완료 되었습니다.");
+        }
+    }
+}
+
+//진행내역 등록
+export const callProgressRegistAPI = ({ registRequest }) => {
+    console.log("진행내역 보내기전의 값");
+    console.log(registRequest);
+    return async (dispatch, getState) => {
+        const result = await request('POST' , `/api/v1/progress`
+            , {'Content-Type' : 'application/json'}
+            , registRequest
+        )
+        console.log('callProgressRegistAPI result.status >>>>>>>>>>>>> '+result.status);
+        if(result.status === 201) {
+            dispatch(postSuccess());
+            toast.info("진행내역이 등록 되었습니다.");
         }
     }
 }
@@ -39,6 +58,43 @@ export const callSalesAPI = ({ salesCode }) => {
     }
 }
 
+//영업 수정
+export const callSalesModifyAPI = ({ salesCode, modifyRequest }) => {
+
+    console.log("수정 보내기전의 값");
+    console.log(modifyRequest);
+
+    return async (dispatch, getState) => {
+
+        const result = await request('PUT',`/api/v1/sales/${salesCode}`
+            , {'Content-Type' : 'application/json'}
+            , modifyRequest);
+
+        console.log('callSalesModifyAPI result : ', result);
+        if(result.status === 201) {
+            dispatch(putSuccess());
+            toast.info("영업 수정이 완료 되었습니다.");
+        }
+    }
+};
+
+//영업 삭제
+export const callSalesDeleteAPI = ({ salesCode}) => {
+
+    return async (dispatch, getState) => {
+
+        const result = await request('DELETE',`/api/v1/sales/${salesCode}`
+            , {'Content-Type' : 'application/json'}
+        );
+
+        console.log('callSalesDeleteAPI result : ', result);
+        if(result.status === 201) {
+            dispatch(deleteSuccess());
+            toast.info("영업 삭제가 완료 되었습니다.");
+        }
+    }
+};
+
 //영업 목록
 export const callSalesListAPI = ({salesStatus, currentPage = 1, schType, schText}) => {
 
@@ -52,6 +108,41 @@ export const callSalesListAPI = ({salesStatus, currentPage = 1, schType, schText
 
         if(result.status === 200) {
             dispatch(getSalesList(result));
+        }
+    }
+};
+
+//상품 목록
+export const callSalesProductListAPI = () => {
+
+    console.log("목록 조회하기전 받은 파라미터");
+    console.log("요청 URL >>>>>>>>>>>> :" +`/api/v1/sales/productList`);
+    return async (dispatch, getState) => {
+
+        const result = await request('GET', `/api/v1/sales/productList`);
+        
+        console.log('callSalesProductListAPI result : ', result);
+
+        if(result.status === 200) {
+            dispatch(getSalesProductList(result));
+        }
+    }
+};
+
+
+//영업 통계
+export const callsalesStatisticsAPI = () => {
+
+    console.log("목록 조회하기전 받은 파라미터");
+    console.log("요청 URL >>>>>>>>>>>> :" +`/api/v1/sales/salesStatistics/all`);
+    return async (dispatch, getState) => {
+
+        const result = await request('GET', `/api/v1/sales/salesStatistics/all`);
+        
+        console.log('callsalesStatisticsAPI result : ', result);
+
+        if(result.status === 200) {
+            dispatch(getSalesStatistics(result));
         }
     }
 };
