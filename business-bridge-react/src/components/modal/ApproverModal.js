d95240df5da959d45cc2dc2d75b3e7dbb148a881
jooseohyeon <wntjguswkd@gmail.com>
2023-12-12 17:46:13 +0900

    feat/#6/등록 모달 구현중,,

--- business-bridge-react/src/components/modal/ApproverModal.js
+++ business-bridge-react/src/components/modal/ApproverModal.js
@@ -1,38 +1,40 @@
-import {useEffect, useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {callAppEmployeeAPI} from "../../apis/ApprovalAPICalls";
-import {el} from "date-fns/locale";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { callAppEmployeeAPI } from "../../apis/ApprovalAPICalls";
 
-function ApproverModal({setAppEmplyModal}) {
-
+function ApproverModal({ setAppEmplyModal }) {
     const dispatch = useDispatch();
-    const {allEmplys} = useSelector(state => state.approvalReducer);
+    const { allEmplys } = useSelector((state) => state.approvalReducer);
 
-    const [selectedEmplys, setSelectedEmplys] = useState([]);
-
-    const onClickApproverCancle = () => {
-        setAppEmplyModal(false);
-    }
-
-    const onClickEmp = (empCode) => {
-        const isEmpSelected = selectedEmplys.includes(empCode);
-        // 직원이 이미 선택되었는지 확인
-
-        if(isEmpSelected) {
-            const updateSelection = selectedEmplys.filter(
-                (selectedEmp) => selectedEmp !== empCode
-            );
-            setSelectedEmplys(updateSelection);
-        } else {
-            setSelectedEmplys([...selectedEmplys, empCode]);
-        }
-    };
+    const [selectedEmployees, setSelectedEmployees] = useState([]);
 
     useEffect(() => {
         dispatch(callAppEmployeeAPI());
     }, []);
 
-    return(
+    const onClickApproverCancle = () => {
+        setAppEmplyModal(false);
+    };
+
+    const onClickEmployee = (empCode) => {
+        const clickedEmployee = allEmplys.find(
+            (appEmp) => appEmp.emplyCode === empCode
+        );
+
+        // 최대 세 명까지만 선택 허용
+        if (selectedEmployees.length < 3) {
+            setSelectedEmployees([...selectedEmployees, clickedEmployee]);
+        }
+    };
+
+    const removeSelectedEmployee = (empCode) => {
+        const updatedSelection = selectedEmployees.filter(
+            (selectedEmp) => selectedEmp.emplyCode !== empCode
+        );
+        setSelectedEmployees(updatedSelection);
+    };
+
+    return (
         <div className="modal">
             <div className="app-modal-container">
                 <h2>결재자 선택</h2>
@@ -40,39 +42,48 @@ function ApproverModal({setAppEmplyModal}) {
                     <div className="app-modal-left">
                         <h3>전체 직원</h3>
                         <div className="app-emply-list">
-                            {
-                                allEmplys &&
-                                allEmplys.map(appEmp => (
-                                    <div className={`app-employee ${
-                                        selectedEmplys.includes(appEmp.emplyCode)
-                                            ? "selected"
-                                            : ""
-                                    }`}
-                                         key={allEmplys.emplyCode}
-                                         onClick={() => onClickEmp(appEmp.emplyCode)}
+                            {allEmplys &&
+                                allEmplys.map((appEmp) => (
+                                    <div
+                                        className={`app-employee ${
+                                            selectedEmployees.some(
+                                                (selectedEmp) => selectedEmp.emplyCode === appEmp.emplyCode
+                                            )
+                                                ? "selected"
+                                                : ""
+                                        }`}
+                                        key={appEmp.emplyCode}
+                                        onClick={() => onClickEmployee(appEmp.emplyCode)}
                                     >
                                         {appEmp.emplyName} {appEmp.positionName}
                                     </div>
-                                ))
-                            }
+                                ))}
                         </div>
                     </div>
                     <div className="app-modal-right">
                         <h3>결재자</h3>
                         <div className="choosen-approver">
-                            {
-                                selectedEmplys &&
-                                selectedEmplys.map((empCode) => (
-                                    <div key={empCode}>{empCode}</div>
-                                ))
-                            }
+                            {selectedEmployees.map((selectedEmp) => (
+                                <div
+                                    key={selectedEmp.emplyCode}
+                                    className="choosen-emp-btn-div"
+                                >
+                                    <div className="app-employee">
+                                        {selectedEmp.emplyName} {selectedEmp.positionName}{" "}
+                                    </div>
+                                    <button
+                                        onClick={() => removeSelectedEmployee(selectedEmp.emplyCode)}
+                                    >
+                                        삭제
+                                    </button>
+                                </div>
+                            ))}
                         </div>
                         <button className="app-up">&#9650;</button>
                         <button className="app-down">&#9660;</button>
-                        <button
-                            className="app-cancle"
-                            onClick={onClickApproverCancle}
-                        >취소</button>
+                        <button className="app-cancle" onClick={onClickApproverCancle}>
+                            취소
+                        </button>
                         <button className="app-use">적용</button>
                     </div>
                 </div>
