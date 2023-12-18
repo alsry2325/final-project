539183165ec608b9a8137bbb0d4ec6bd5c8c7b81
asoong2 <yang950454@gmail.com>
2023-12-18 23:32:28 +0900

    주소록, 쪽지 사원 이미지 조회 완료

--- business-bridge-react/src/components/items/AddressBookModal.js
+++ business-bridge-react/src/components/items/AddressBookModal.js
@@ -45,7 +45,10 @@ function AddressBookModal({
                             <div className="note-modal-address-info" key={index}
                                  onClick={() => handleSelectRecipient(item.emplyCode, item.emplyName)}>
                                 <div className="note-modal-detail">
-                                    {item.emplyName} {item.departmentName} {item.positionName} {item.emplyEmail}
+                                    <img src={item.emplyPhoto}/>
+                                    <div className="note-modal-name">{item.emplyName}</div>
+                                    <div className="note-modal-deptName">{item.departmentName}</div>
+                                    <div className="note-modal-position">{item.positionName}</div>
                                 </div>
                             </div>
                         ))}
