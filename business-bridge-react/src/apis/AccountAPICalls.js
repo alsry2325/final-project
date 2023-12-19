2ad4b26dc9715c1c408ca314c3484ed7cd751e75
jung su hyun <bm4564@naver.com>
2023-12-19 09:47:41 +0900

    feat#40 account API변경

--- business-bridge-react/src/apis/AccountAPICalls.js
+++ business-bridge-react/src/apis/AccountAPICalls.js
@@ -13,10 +13,14 @@ export const callAccountRegistAPI = ({ registRequest }) => {
     console.log("거래처등록 보내기전의 값");
     console.log(registRequest);
     return async (dispatch, getState) => {
-        const result = await request('POST' , `/api/v1/account`
-            , {'Content-Type' : 'application/json'}
+
+        const result = await authRequest.post(`/api/v1/account`
             , registRequest
-        )
+            , {'Content-Type' : 'application/json'}
+        ).catch(e => {
+                console.log(e.response.status);
+            }
+        );
 
         console.log('callAccountRegistAPI result.status >>>>>>>>>>>>> '+result.status);
         if(result.status === 201) {
@@ -33,7 +37,15 @@ export const callAccountAPI = ({ accountCode }) => {
 
     return async (dispatch, getState) => {
 
-        const result = await request('GET',`/api/v1/account/${accountCode}`);
+        const result = await authRequest.get(`/api/v1/account/${accountCode}`,
+            {
+                headers : {
+                    'Content-Type' : 'application/json'
+                }
+            }).catch(e => {
+            console.log(e);
+        });
+
         console.log('callAccountAPI result : ', result);
         if(result?.status === 200) {
             dispatch(getAccount(result));
@@ -51,10 +63,7 @@ export const callAccountModifyAPI = ({ accountCode, modifyRequest }) => {
 
     return async (dispatch, getState) => {
 
-        const result = await request('PUT',`/api/v1/account/${accountCode}`
-            , {'Content-Type' : 'application/json'}
-            , modifyRequest);
-
+        const result = await authRequest.put(`/api/v1/account/${accountCode}`, modifyRequest);
         console.log('callAccountModifyAPI result : ', result);
         if(result.status === 201) {
             dispatch(putSuccess());
@@ -68,9 +77,7 @@ export const callAccountDeleteAPI = ({ accountCode}) => {
 
     return async (dispatch, getState) => {
 
-        const result = await request('DELETE',`/api/v1/account/${accountCode}`
-            , {'Content-Type' : 'application/json'}
-        );
+        const result = await authRequest.delete(`/api/v1/account/${accountCode}`);
 
         console.log('callAccountDeleteAPI result : ', result);
         if(result.status === 201) {
@@ -86,12 +93,16 @@ export const callAccountListAPI = ({departmentCode, currentPage = 1, schType, sc
     console.log("목록 조회하기전 받은 파라미터");
     console.log("요청 URL >>>>>>>>>>>> :" +`/api/v1/account/accountList/${departmentCode}?page=${currentPage}&schType=${schType}&schText=${schText}`);
     return async (dispatch, getState) => {
-
-        const result = await request('GET', `/api/v1/account/accountList/${departmentCode}?page=${currentPage}&schType=${schType}&schText=${schText}`);
-
+        const result = await authRequest.get(`/api/v1/account/accountList/${departmentCode}?page=${currentPage}&schType=${schType}&schText=${schText}`,
+            {
+                headers : {
+                    'Content-Type' : 'application/json'
+                }
+            }).catch(e => {
+            console.log(e);
+        });
         console.log('callAccountListAPI result : ', result);
-
-        if(result.status === 200) {
+        if(result?.status === 200) {
             dispatch(getAccountList(result));
         }
     }
